1033ddaf65d7bd15745f8081ac7253cb342a72f2
JakeAHaskell <jahaskell@ufl.edu>
2019-09-06 15:59:50 -0400

    First commit

--- server.js
+++ server.js
@@ -9,43 +9,39 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  //request handler
+  if (request.method === "GET" && parsedUrl.pathname === "/listings") {
+    //return success code
+    response.statusCode = 200;
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+    //responds with data
+    response.write(listingData);
+  }
+  else {
+    //send bad request code
+    response.statusCode = 404;
+    response.write("Bad gateway error");
+  }
+  //tells server body and headers have been sent
+  response.end();
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
-  
-
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
-
+  listingData = data;
+  //calls function to start server
+  startServer();
 });
+
+//function to start server
+function startServer() {
+  //creates the server
+  server = http.createServer(requestHandler);
+
+  // listens on port specified
+  server.listen(port, function () {
+
+  //console log that server is listening
+    console.log("Server listening on: http://127.0.0.1:" + port);
+
+  });
+}
